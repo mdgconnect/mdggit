0d9a9046a92d8a11d21e67326c6e784dc0012512
mdgconnect <moumita.dasgupta@mercedes-benz.com>
2025-12-01 17:10:59 +0530

    Update streamlit_app_final.py

--- streamlit_app_final.py
+++ streamlit_app_final.py
@@ -162,7 +162,7 @@ with tabs[2]:
 # Dealer Analysis Tab
 with tabs[3]:
     st.subheader("Dealer-Level Analysis")
-    dealer_data = filtered.groupby(['DealerID','Country']).agg(Rate=('is_delinquent','mean')).reset_index().rename(columns={'dealerbpid': 'DealerID'}]
+    dealer_data = filtered.groupby(['DealerID','Country']).agg(Rate=('is_delinquent','mean')).reset_index().rename(columns={'DealerID': 'Dealer ID'}]
     dealer_data['Rate'] = dealer_data['Rate']*100
     fig_dealer = px.bar(dealer_data, x='DealerID', y='Rate', color='Country', title='Dealer-Level Delinquency Rate')
     st.plotly_chart(fig_dealer, use_container_width=True)

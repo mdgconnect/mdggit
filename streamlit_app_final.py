2c5eeb38f0726e815d89981bb937be89bcb7e273
mdgconnect <moumita.dasgupta@mercedes-benz.com>
2025-12-02 12:07:52 +0530

    Update streamlit_app_final.py

--- streamlit_app_final.py
+++ streamlit_app_final.py
@@ -68,7 +68,7 @@ date_range = st.sidebar.date_input("Date Range", value=(all_df['Event'].min().da
 model_filter = st.sidebar.multiselect("Vehicle Model", options=sorted(all_df['modeldescription'].dropna().unique()), default=[], key="model_filter")
 fuel_filter = st.sidebar.multiselect(
     "fueltypecode",
-    options=sorted(all_df['fueltypecode'].dropna().unique()),
+    options=sorted(all_df['FuelType'].dropna().unique()),
     default=[],
     key="fuel_filter")
 model_search = st.sidebar.text_input("Search Model", value="", key="model_search")
